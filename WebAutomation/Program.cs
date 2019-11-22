2fa6769f60db199e108d28a0c09ee3e959caf93e
Hansika Wanniarachchi <hansijw76@gmail.com>
2019-11-22 20:17:28 +0530

    initial commit

---
+++ WebAutomation/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:4200/user/login");
+            driver.FindElement(By.Name("UserName")).SendKeys("hansi");
+            driver.FindElement(By.Name("Password")).SendKeys("0987");
+        }
+    }
+}
